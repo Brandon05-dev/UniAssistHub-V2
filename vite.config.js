3fd87430d3882728a34c7baa72c5d5317d4eb6a4
Brandon <omutitibrandon@gmail.com>
2025-05-28 08:31:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: 'all',  // Allow all hosts to fix the blocked request issue
   },
-  preview: {
-    allowedHosts: [
-      'uniassisthub-v2-q5o8.onrender.com'
-    ]
-  }
 });
